d9502b471804844a6f1391156b71139629a4058d
orbbec <liuhua@orbbec.com>
2016-11-03 23:47:40 +0800

    add soft library

---
+++ astra_openni2/Bin/Intermediate/Arm-Release/NiViewer/Device.d
@@ -0,0 +1,321 @@
+../../../Bin/Intermediate/Arm-Release/NiViewer/Device.d ../../../Bin/Intermediate/Arm-Release/NiViewer/Device.o: \
+ Device.cpp /usr/include/stdc-predef.h ../../../Include/OpenNI.h \
+ ../../../Include/OniPlatform.h \
+ ../../../Include/Linux-Arm/OniPlatformLinux-Arm.h \
+ ../../../Include/Linux-Arm/../Linux-x86/OniPlatformLinux-x86.h \
+ /usr/include/unistd.h /usr/include/features.h \
+ /usr/include/arm-linux-gnueabihf/sys/cdefs.h \
+ /usr/include/arm-linux-gnueabihf/bits/wordsize.h \
+ /usr/include/arm-linux-gnueabihf/gnu/stubs.h \
+ /usr/include/arm-linux-gnueabihf/gnu/stubs-hard.h \
+ /usr/include/arm-linux-gnueabihf/bits/posix_opt.h \
+ /usr/include/arm-linux-gnueabihf/bits/environments.h \
+ /usr/include/arm-linux-gnueabihf/bits/types.h \
+ /usr/include/arm-linux-gnueabihf/bits/typesizes.h \
+ /usr/lib/gcc/arm-linux-gnueabihf/4.8/include/stddef.h \
+ /usr/include/arm-linux-gnueabihf/bits/confname.h /usr/include/getopt.h \
+ /usr/include/arm-linux-gnueabihf/bits/unistd.h /usr/include/stdlib.h \
+ /usr/include/arm-linux-gnueabihf/bits/waitflags.h \
+ /usr/include/arm-linux-gnueabihf/bits/waitstatus.h /usr/include/endian.h \
+ /usr/include/arm-linux-gnueabihf/bits/endian.h \
+ /usr/include/arm-linux-gnueabihf/bits/byteswap.h \
+ /usr/include/arm-linux-gnueabihf/bits/byteswap-16.h \
+ /usr/include/xlocale.h /usr/include/arm-linux-gnueabihf/sys/types.h \
+ /usr/include/time.h /usr/include/arm-linux-gnueabihf/sys/select.h \
+ /usr/include/arm-linux-gnueabihf/bits/select.h \
+ /usr/include/arm-linux-gnueabihf/bits/sigset.h \
+ /usr/include/arm-linux-gnueabihf/bits/time.h \
+ /usr/include/arm-linux-gnueabihf/bits/select2.h \
+ /usr/include/arm-linux-gnueabihf/sys/sysmacros.h \
+ /usr/include/arm-linux-gnueabihf/bits/pthreadtypes.h \
+ /usr/include/alloca.h \
+ /usr/include/arm-linux-gnueabihf/bits/stdlib-bsearch.h \
+ /usr/include/arm-linux-gnueabihf/bits/stdlib-float.h \
+ /usr/include/arm-linux-gnueabihf/bits/stdlib.h /usr/include/stdio.h \
+ /usr/include/libio.h /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/lib/gcc/arm-linux-gnueabihf/4.8/include/stdarg.h \
+ /usr/include/arm-linux-gnueabihf/bits/stdio_lim.h \
+ /usr/include/arm-linux-gnueabihf/bits/sys_errlist.h \
+ /usr/include/arm-linux-gnueabihf/bits/stdio.h \
+ /usr/include/arm-linux-gnueabihf/bits/stdio2.h /usr/include/string.h \
+ /usr/include/arm-linux-gnueabihf/bits/string3.h /usr/include/assert.h \
+ /usr/lib/gcc/arm-linux-gnueabihf/4.8/include-fixed/limits.h \
+ /usr/lib/gcc/arm-linux-gnueabihf/4.8/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/arm-linux-gnueabihf/bits/posix1_lim.h \
+ /usr/include/arm-linux-gnueabihf/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/arm-linux-gnueabihf/bits/posix2_lim.h \
+ /usr/include/arm-linux-gnueabihf/bits/xopen_lim.h \
+ /usr/lib/gcc/arm-linux-gnueabihf/4.8/include/stdint.h \
+ /usr/include/stdint.h /usr/include/arm-linux-gnueabihf/bits/wchar.h \
+ ../../../Include/OniProperties.h ../../../Include/OniEnums.h \
+ ../../../Include/OniCAPI.h ../../../Include/OniCTypes.h \
+ ../../../Include/OniCEnums.h ../../../Include/OniCProperties.h \
+ ../../../Include/OniVersion.h Device.h Draw.h \
+ ../../../ThirdParty/GL/GL/gl.h ../../../ThirdParty/GL/GL/glext.h \
+ /usr/include/math.h /usr/include/arm-linux-gnueabihf/bits/huge_val.h \
+ /usr/include/arm-linux-gnueabihf/bits/huge_valf.h \
+ /usr/include/arm-linux-gnueabihf/bits/huge_vall.h \
+ /usr/include/arm-linux-gnueabihf/bits/inf.h \
+ /usr/include/arm-linux-gnueabihf/bits/nan.h \
+ /usr/include/arm-linux-gnueabihf/bits/mathdef.h \
+ /usr/include/arm-linux-gnueabihf/bits/mathcalls.h \
+ /usr/include/arm-linux-gnueabihf/bits/mathinline.h \
+ /usr/include/arm-linux-gnueabihf/bits/math-finite.h \
+ ../../../ThirdParty/PSCommon/XnLib/Include/XnLog.h \
+ ../../../ThirdParty/PSCommon/XnLib/Include/XnLib.h \
+ ../../../ThirdParty/PSCommon/XnLib/Include/XnPlatform.h \
+ ../../../ThirdParty/PSCommon/XnLib/Include/Linux-Arm/XnPlatformLinux-Arm.h \
+ ../../../ThirdParty/PSCommon/XnLib/Include/Linux-Arm/../Linux-x86/XnPlatformLinux-x86.h \
+ /usr/lib/gcc/arm-linux-gnueabihf/4.8/include/float.h \
+ ../../../ThirdParty/PSCommon/XnLib/Include/XnStatus.h \
+ ../../../ThirdParty/PSCommon/XnLib/Include/XnPlatform.h \
+ ../../../ThirdParty/PSCommon/XnLib/Include/XnStatusCodes.h \
+ ../../../ThirdParty/PSCommon/XnLib/Include/XnOS.h \
+ ../../../ThirdParty/PSCommon/XnLib/Include/XnMacros.h \
+ ../../../ThirdParty/PSCommon/XnLib/Include/XnOSStrings.h \
+ ../../../ThirdParty/PSCommon/XnLib/Include/XnMemory.h \
+ ../../../ThirdParty/PSCommon/XnLib/Include/Linux-x86/XnOSLinux-x86.h \
+ /usr/include/arm-linux-gnueabihf/bits/timex.h /usr/include/pthread.h \
+ /usr/include/sched.h /usr/include/arm-linux-gnueabihf/bits/sched.h \
+ /usr/include/arm-linux-gnueabihf/bits/setjmp.h \
+ /usr/include/arm-linux-gnueabihf/sys/stat.h \
+ /usr/include/arm-linux-gnueabihf/bits/stat.h /usr/include/fcntl.h \
+ /usr/include/arm-linux-gnueabihf/bits/fcntl.h \
+ /usr/include/arm-linux-gnueabihf/bits/fcntl-linux.h \
+ /usr/include/arm-linux-gnueabihf/bits/uio.h \
+ /usr/include/arm-linux-gnueabihf/bits/fcntl2.h \
+ ../../../ThirdParty/PSCommon/XnLib/Include/XnLogTypes.h \
+ ../../../ThirdParty/PSCommon/XnLib/Include/XnDump.h \
+ ../../../Include/PS1080.h ../../../Include/OniCTypes.h \
+ ../../../Include/KinectProperties.h
+
+/usr/include/stdc-predef.h:
+
+../../../Include/OpenNI.h:
+
+../../../Include/OniPlatform.h:
+
+../../../Include/Linux-Arm/OniPlatformLinux-Arm.h:
+
+../../../Include/Linux-Arm/../Linux-x86/OniPlatformLinux-x86.h:
+
+/usr/include/unistd.h:
+
+/usr/include/features.h:
+
+/usr/include/arm-linux-gnueabihf/sys/cdefs.h:
+
+/usr/include/arm-linux-gnueabihf/bits/wordsize.h:
+
+/usr/include/arm-linux-gnueabihf/gnu/stubs.h:
+
+/usr/include/arm-linux-gnueabihf/gnu/stubs-hard.h:
+
+/usr/include/arm-linux-gnueabihf/bits/posix_opt.h:
+
+/usr/include/arm-linux-gnueabihf/bits/environments.h:
+
+/usr/include/arm-linux-gnueabihf/bits/types.h:
+
+/usr/include/arm-linux-gnueabihf/bits/typesizes.h:
+
+/usr/lib/gcc/arm-linux-gnueabihf/4.8/include/stddef.h:
+
+/usr/include/arm-linux-gnueabihf/bits/confname.h:
+
+/usr/include/getopt.h:
+
+/usr/include/arm-linux-gnueabihf/bits/unistd.h:
+
+/usr/include/stdlib.h:
+
+/usr/include/arm-linux-gnueabihf/bits/waitflags.h:
+
+/usr/include/arm-linux-gnueabihf/bits/waitstatus.h:
+
+/usr/include/endian.h:
+
+/usr/include/arm-linux-gnueabihf/bits/endian.h:
+
+/usr/include/arm-linux-gnueabihf/bits/byteswap.h:
+
+/usr/include/arm-linux-gnueabihf/bits/byteswap-16.h:
+
+/usr/include/xlocale.h:
+
+/usr/include/arm-linux-gnueabihf/sys/types.h:
+
+/usr/include/time.h:
+
+/usr/include/arm-linux-gnueabihf/sys/select.h:
+
+/usr/include/arm-linux-gnueabihf/bits/select.h:
+
+/usr/include/arm-linux-gnueabihf/bits/sigset.h:
+
+/usr/include/arm-linux-gnueabihf/bits/time.h:
+
+/usr/include/arm-linux-gnueabihf/bits/select2.h:
+
+/usr/include/arm-linux-gnueabihf/sys/sysmacros.h:
+
+/usr/include/arm-linux-gnueabihf/bits/pthreadtypes.h:
+
+/usr/include/alloca.h:
+
+/usr/include/arm-linux-gnueabihf/bits/stdlib-bsearch.h:
+
+/usr/include/arm-linux-gnueabihf/bits/stdlib-float.h:
+
+/usr/include/arm-linux-gnueabihf/bits/stdlib.h:
+
+/usr/include/stdio.h:
+
+/usr/include/libio.h:
+
+/usr/include/_G_config.h:
+
+/usr/include/wchar.h:
+
+/usr/lib/gcc/arm-linux-gnueabihf/4.8/include/stdarg.h:
+
+/usr/include/arm-linux-gnueabihf/bits/stdio_lim.h:
+
+/usr/include/arm-linux-gnueabihf/bits/sys_errlist.h:
+
+/usr/include/arm-linux-gnueabihf/bits/stdio.h:
+
+/usr/include/arm-linux-gnueabihf/bits/stdio2.h:
+
+/usr/include/string.h:
+
+/usr/include/arm-linux-gnueabihf/bits/string3.h:
+
+/usr/include/assert.h:
+
+/usr/lib/gcc/arm-linux-gnueabihf/4.8/include-fixed/limits.h:
+
+/usr/lib/gcc/arm-linux-gnueabihf/4.8/include-fixed/syslimits.h:
+
+/usr/include/limits.h:
+
+/usr/include/arm-linux-gnueabihf/bits/posix1_lim.h:
+
+/usr/include/arm-linux-gnueabihf/bits/local_lim.h:
+
+/usr/include/linux/limits.h:
+
+/usr/include/arm-linux-gnueabihf/bits/posix2_lim.h:
+
+/usr/include/arm-linux-gnueabihf/bits/xopen_lim.h:
+
+/usr/lib/gcc/arm-linux-gnueabihf/4.8/include/stdint.h:
+
+/usr/include/stdint.h:
+
+/usr/include/arm-linux-gnueabihf/bits/wchar.h:
+
+../../../Include/OniProperties.h:
+
+../../../Include/OniEnums.h:
+
+../../../Include/OniCAPI.h:
+
+../../../Include/OniCTypes.h:
+
+../../../Include/OniCEnums.h:
+
+../../../Include/OniCProperties.h:
+
+../../../Include/OniVersion.h:
+
+Device.h:
+
+Draw.h:
+
+../../../ThirdParty/GL/GL/gl.h:
+
+../../../ThirdParty/GL/GL/glext.h:
+
+/usr/include/math.h:
+
+/usr/include/arm-linux-gnueabihf/bits/huge_val.h:
+
+/usr/include/arm-linux-gnueabihf/bits/huge_valf.h:
+
+/usr/include/arm-linux-gnueabihf/bits/huge_vall.h:
+
+/usr/include/arm-linux-gnueabihf/bits/inf.h:
+
+/usr/include/arm-linux-gnueabihf/bits/nan.h:
+
+/usr/include/arm-linux-gnueabihf/bits/mathdef.h:
+
+/usr/include/arm-linux-gnueabihf/bits/mathcalls.h:
+
+/usr/include/arm-linux-gnueabihf/bits/mathinline.h:
+
+/usr/include/arm-linux-gnueabihf/bits/math-finite.h:
+
+../../../ThirdParty/PSCommon/XnLib/Include/XnLog.h:
+
+../../../ThirdParty/PSCommon/XnLib/Include/XnLib.h:
+
+../../../ThirdParty/PSCommon/XnLib/Include/XnPlatform.h:
+
+../../../ThirdParty/PSCommon/XnLib/Include/Linux-Arm/XnPlatformLinux-Arm.h:
+
+../../../ThirdParty/PSCommon/XnLib/Include/Linux-Arm/../Linux-x86/XnPlatformLinux-x86.h:
+
+/usr/lib/gcc/arm-linux-gnueabihf/4.8/include/float.h:
+
+../../../ThirdParty/PSCommon/XnLib/Include/XnStatus.h:
+
+../../../ThirdParty/PSCommon/XnLib/Include/XnPlatform.h:
+
+../../../ThirdParty/PSCommon/XnLib/Include/XnStatusCodes.h:
+
+../../../ThirdParty/PSCommon/XnLib/Include/XnOS.h:
+
+../../../ThirdParty/PSCommon/XnLib/Include/XnMacros.h:
+
+../../../ThirdParty/PSCommon/XnLib/Include/XnOSStrings.h:
+
+../../../ThirdParty/PSCommon/XnLib/Include/XnMemory.h:
+
+../../../ThirdParty/PSCommon/XnLib/Include/Linux-x86/XnOSLinux-x86.h:
+
+/usr/include/arm-linux-gnueabihf/bits/timex.h:
+
+/usr/include/pthread.h:
+
+/usr/include/sched.h:
+
+/usr/include/arm-linux-gnueabihf/bits/sched.h:
+
+/usr/include/arm-linux-gnueabihf/bits/setjmp.h:
+
+/usr/include/arm-linux-gnueabihf/sys/stat.h:
+
+/usr/include/arm-linux-gnueabihf/bits/stat.h:
+
+/usr/include/fcntl.h:
+
+/usr/include/arm-linux-gnueabihf/bits/fcntl.h:
+
+/usr/include/arm-linux-gnueabihf/bits/fcntl-linux.h:
+
+/usr/include/arm-linux-gnueabihf/bits/uio.h:
+
+/usr/include/arm-linux-gnueabihf/bits/fcntl2.h:
+
+../../../ThirdParty/PSCommon/XnLib/Include/XnLogTypes.h:
+
+../../../ThirdParty/PSCommon/XnLib/Include/XnDump.h:
+
+../../../Include/PS1080.h:
+
+../../../Include/OniCTypes.h:
+
+../../../Include/KinectProperties.h:
