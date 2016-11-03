d1a7d88ba0ac67b532843fff2ee0163c048ba394
orbbec <liuhua@orbbec.com>
2016-11-03 23:55:53 +0800

    delete compiled files

--- astra_openni2/Bin/Intermediate/x64-Release/liborbbec.so/XnAudioProcessor.d
+++
@@ -1,4 +0,0 @@
-../../../Bin/Intermediate/x64-Release/liborbbec.so/XnAudioProcessor.d ../../../Bin/Intermediate/x64-Release/liborbbec.so/XnAudioProcessor.o: \
- Sensor/XnAudioProcessor.cpp /usr/include/stdc-predef.h
-
-/usr/include/stdc-predef.h:
--- astra_openni2/Bin/Intermediate/x64-Release/liborbbec.so/XnSensorAudioStream.d
+++
@@ -1,4 +0,0 @@
-../../../Bin/Intermediate/x64-Release/liborbbec.so/XnSensorAudioStream.d ../../../Bin/Intermediate/x64-Release/liborbbec.so/XnSensorAudioStream.o: \
- Sensor/XnSensorAudioStream.cpp /usr/include/stdc-predef.h
-
-/usr/include/stdc-predef.h:
